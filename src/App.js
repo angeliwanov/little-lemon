e1f5c1beacfce3a114541247e1630b7e1492e851
angeliwanov <angel.iwanov@gmail.com>
2023-07-05 12:05:43 +0300

    semantic html

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <React.Fragment>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import logo from "../assets/Logo.svg";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="little lemon logo"></img>
+    </header>
+  );
+};
+
+export default Header;
