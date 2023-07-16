f127ddde0ce7b1da2b1d90d71648c70adaed4b7a
angeliwanov <angel.iwanov@gmail.com>
2023-07-16 11:49:27 +0300

    BookingForm

---
+++ src/components/BookingForm.js
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+function BookingForm({ availableTimes }) {
+  const [reservations, setReservations] = useState([]);
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [guests, setGuests] = useState(0);
+  const [occasion, setOccasion] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!date || !time || !guests) return;
+    let newReservation = {
+      date,
+      time,
+      guests,
+      occasion,
+    };
+    setReservations([...reservations, newReservation]);
+    setDate("");
+    setTime("");
+    setGuests(0);
+    setOccasion("");
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      style={{
+        display: "grid",
+        maxWidth: "200px",
+        gap: "20px",
+        marginBottom: "20px",
+      }}
+    >
+      <label htmlFor="res-date">Choose date</label>
+      <input
+        required
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+        type="date"
+        id="res-date"
+      ></input>
+      <label htmlFor="res-time">Choose time</label>
+      <select
+        required
+        id="res-time "
+        value={time}
+        name=""
+        onChange={(e) => setTime(e.target.value)}
+      >
+        <option selected value="">
+          -- Please select time --
+        </option>
+        {availableTimes.map((time) => (
+          <option key={time}>{time}</option>
+        ))}
+      </select>
+      <label htmlFor="guests">Number of guests</label>
+      <input
+        required
+        onChange={(e) => setGuests(Number(e.target.value))}
+        type="number"
+        value={guests}
+        min="1"
+        max="10"
+        id="guests"
+      ></input>
+      <label htmlFor="occasion">Occasion</label>
+      <input
+        placeholder="Birthday, Anniversary ..."
+        text="text"
+        id="occasion"
+        onChange={(e) => setOccasion(e.target.value)}
+      ></input>
+      <button type="submit" value="Make Your reservation">
+        Reserve
+      </button>
+    </form>
+  );
+}
+
+export default BookingForm;
--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -1,5 +1,10 @@
+import BookingForm from "../components/BookingForm";
+import "../styles/BookingPage.css";
+
 function BookingPage() {
-  return <div>BookingPage</div>;
+  const availableTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+
+  return <BookingForm availableTimes={availableTimes} />;
 }
 
 export default BookingPage;
